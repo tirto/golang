f0968fa43fe796cefb7023120d1bb133e66314c0
Tirto Adji <tirtoadji@gmail.com>
2014-01-13 04:33:22 -0600

    formatted

--- webserver.go
+++ webserver.go
@@ -3,9 +3,9 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
-	"io/ioutil"
 )
 
 type userProfileReq struct {
@@ -14,8 +14,8 @@ type userProfileReq struct {
 }
 
 func printError(msg string, rw http.ResponseWriter) {
-    rw.WriteHeader(500)
-    fmt.Fprintf(rw, "{\"status\":{\"code\":500,\"description\":\"%s\"}}\n", msg)
+	rw.WriteHeader(500)
+	fmt.Fprintf(rw, "{\"status\":{\"code\":500,\"description\":\"%s\"}}\n", msg)
 }
 
 func handleUserProfile(res http.ResponseWriter, req *http.Request) {
@@ -24,35 +24,35 @@ func handleUserProfile(res http.ResponseWriter, req *http.Request) {
 	err := decoder.Decode(&u)
 	if err != nil {
 		log.Println(err.Error())
-        errorDesc := "Error: unable to parse request"
-        printError(errorDesc, res)
-        return
+		errorDesc := "Error: unable to parse request"
+		printError(errorDesc, res)
+		return
 	}
 
-	log.Println("apiKey =",u.ApiKey)
-	log.Println("id =",u.Id)
+	log.Println("apiKey =", u.ApiKey)
+	log.Println("id =", u.Id)
 	apiKey := u.ApiKey
 	id := u.Id
-    // TODO: implement isApiKeyValid(apiKey)
+	// TODO: implement isApiKeyValid(apiKey)
 	if apiKey != "dd06058e-f32a-4e11-b14b-85a2f98ea523" {
-        errorDesc := "Error: invalid apiKey"
-        printError(errorDesc, res)
-        return
+		errorDesc := "Error: invalid apiKey"
+		printError(errorDesc, res)
+		return
 	}
 
-    // TODO: implement lookupUserProfile(id)
-    if id != "abcxyz123" { 
-        errorDesc := "Error: unable to parse request"
-        printError(errorDesc, res)
-        return
+	// TODO: implement lookupUserProfile(id)
+	if id != "abcxyz123" {
+		errorDesc := "Error: unable to parse request"
+		printError(errorDesc, res)
+		return
 	}
 
 	// no input errors
 	buf, err := ioutil.ReadFile("/home/tirto/go/src/github.com/tirto/webserver/userProfile.json")
 	if err != nil {
-        errorDesc := "Error: unable to lookup userProfile"
-        printError(errorDesc, res)
-        return
+		errorDesc := "Error: unable to lookup userProfile"
+		printError(errorDesc, res)
+		return
 	}
 	fmt.Fprintf(res, string(buf))
 }
