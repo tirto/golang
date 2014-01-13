77a82288d7cf529c71126c5888e05c7bc015e391
Tirto Adji <tirtoadji@gmail.com>
2014-01-13 04:40:28 -0600

    fixed indent width to 4 spaces

--- hello.go
+++ hello.go
@@ -1,12 +1,12 @@
 // test package
-// 
+//
 package main
 
 import (
-   "fmt"
-   "github.com/tirto/newmath"
+	"fmt"
+	"github.com/tirto/newmath"
 )
 
 func main() {
-    fmt.Printf("hello, world. Sqrt(2) = %v\n", newmath.Sqrt(2))
+	fmt.Printf("hello, world. Sqrt(2) = %v\n", newmath.Sqrt(2))
 }
