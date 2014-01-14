0872a0fabe2de04f794343bacb2368e2476b56f8
Tirto Adji <tirtoadji@gmail.com>
2014-01-14 08:10:58 -0600

    added userProfileLookup()

--- webserver.go
+++ webserver.go
@@ -8,6 +8,25 @@ import (
     "net/http"
 )
 
+type userProfileLookupReq struct {
+    ApiKey string
+    Verbose bool
+    Lookup_by struct {
+        Interests struct {
+            Interest []string
+        }
+        Keywords struct {
+            Keyword []string
+        }
+        Categories struct {
+            Category []string
+        }
+        Tags struct {
+            Tag map[string]string
+        }
+    }
+}
+
 type userProfileReq struct {
     ApiKey string
     Id     string
@@ -16,6 +35,56 @@ type userProfileReq struct {
 func printError(msg string, rw http.ResponseWriter) {
     rw.WriteHeader(500)
     fmt.Fprintf(rw, "{\"status\":{\"code\":500,\"description\":\"%s\"}}\n", msg)
+}
+
+func handleUserProfileLookup(res http.ResponseWriter, req *http.Request) {
+    decoder := json.NewDecoder(req.Body)
+    var u userProfileLookupReq
+    err := decoder.Decode(&u)
+    if err != nil {
+        log.Println(err.Error())
+        errorDesc := "Error: unable to parse request"
+        printError(errorDesc, res)
+        return
+    }
+
+    apiKey := u.ApiKey
+    verbose := u.Verbose
+    lookup_by := u.Lookup_by
+    interests := lookup_by.Interests
+    categories := lookup_by.Categories
+    tags := lookup_by.Tags
+    keywords := lookup_by.Keywords
+    log.Println("apiKey = ",apiKey)
+    log.Println("verbose = ",verbose)
+    log.Println("lookup_by = ",lookup_by)
+    log.Println("interests=", interests)
+    log.Println("categories=", categories)
+    log.Println("tags=", tags)
+    log.Println("keywords=", keywords)
+
+
+
+    // no input errors
+    filename := "/home/tirto/go/src/github.com/tirto/webserver/userProfileLookup.json"
+    if verbose {
+        filename = "/home/tirto/go/src/github.com/tirto/webserver/userProfileLookupVerbose.json"
+    }
+
+    // TODO: implement isApiKeyValid(apiKey)
+    if apiKey != "dd06058e-f32a-4e11-b14b-85a2f98ea523" {
+        errorDesc := "Error: invalid apiKey"
+        printError(errorDesc, res)
+        return
+    }
+
+    buf, err := ioutil.ReadFile(filename)
+    if err != nil {
+        errorDesc := "Error: unable to lookup userProfile"
+        printError(errorDesc, res)
+        return
+    }
+    fmt.Fprintf(res, string(buf))
 }
 
 func handleUserProfile(res http.ResponseWriter, req *http.Request) {
@@ -60,5 +129,6 @@ func handleUserProfile(res http.ResponseWriter, req *http.Request) {
 // main function
 func main() {
     http.HandleFunc("/AudService/v1/user/profile", handleUserProfile)
+    http.HandleFunc("/AudService/v1/user/profile/lookup", handleUserProfileLookup)
     log.Fatal(http.ListenAndServe(":8080", nil))
 }
